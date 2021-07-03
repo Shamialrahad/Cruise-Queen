bd5550bb98bd2a38fab2c5dd2788ae36d961e3c3
Shamialrahad <rahad15-7349@diu.edu.bd>
2021-07-03 15:07:37 +0600

    add javascript to ninja ticket

---
+++ index.js
@@ -0,0 +1,44 @@
+//select class ticket function
+function selectTicketChange(ticket,isIncrease){
+    const ticketCount = getInputValue(ticket);
+    let ticketNewCount = ticketCount;
+    if(isIncrease == true){
+        ticketNewCount = ticketCount + 1;
+    }
+    if(isIncrease == false && ticketCount > 0){
+        ticketNewCount = ticketCount - 1;
+    }
+    document.getElementById(ticket+'-count').value = ticketNewCount;
+    let ticketTotal = 0;
+    if(ticket == 'firstclass'){
+        ticketTotal = ticketNewCount*150;
+    }
+    if(ticket == 'economyclass'){
+        ticketTotal = ticketNewCount*100;
+    }
+    document.getElementById(ticket+'-total').innerText = ticketTotal;
+    calculateTotal();
+}
+
+
+//total price calculation function
+function calculateTotal(){
+    const firstCount = getInputValue('firstclass');
+    const economy = getInputValue('economyclass');
+
+    const totalPrice = firstCount*150 + economy*100;
+    document.getElementById('total-price').innerText = totalPrice;
+
+    const tax = Math.round(totalPrice*0.1);
+    document.getElementById('tax-amount').innerText = tax;
+
+    const grandTotal = totalPrice+tax;
+    document.getElementById('grand-total').innerText = grandTotal;
+}
+
+//get input ticket price function
+function getInputValue(ticket){
+    const ticketInput = document.getElementById(ticket+'-count');
+    const ticketCount = parseInt(ticketInput.value);
+    return ticketCount;
+}
